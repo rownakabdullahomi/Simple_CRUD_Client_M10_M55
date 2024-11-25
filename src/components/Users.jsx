0a0aecd39dfceac96ded1d78434b82a54d381aa6
rownakabdullahomi <rownakabdullahomi@gmail.com>
2024-11-25 14:39:34 +0600

    update operation done

--- src/components/Users.jsx
+++ src/components/Users.jsx
@@ -1,7 +1,9 @@
-import { useLoaderData } from "react-router-dom";
+import { useState } from "react";
+import { Link, useLoaderData } from "react-router-dom";
 
 const Users = () => {
-  const users = useLoaderData();
+  const loadedUsers = useLoaderData();
+  const [users, setUsers] = useState(loadedUsers)
 
   const handleDelete = (_id)=>{
     console.log(_id);
@@ -13,6 +15,8 @@ const Users = () => {
         console.log(data);
         if(data.deletedCount>0){
             alert("Deleted");
+            const remaining = users.filter(user => user._id !== _id);
+            setUsers(remaining);
         }
 
     })
@@ -24,6 +28,7 @@ const Users = () => {
         {users.map((user) => (
           <p key={user._id}>
             {user.name} : {user.email}  {user._id}
+            <Link to={`/update/${user._id}`}>  <button>Update</button>  </Link>
             <button onClick={()=>handleDelete(user._id)}>X</button>
           </p>
         ))}
--- src/main.jsx
+++ src/main.jsx
@@ -5,6 +5,7 @@ import App from "./App.jsx";
 
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import Users from "./components/users.jsx";
+import Update from "./components/Update.jsx";
 
 const router = createBrowserRouter([
   {
@@ -15,6 +16,11 @@ const router = createBrowserRouter([
     path: "/users",
     element: <Users></Users>,
     loader: ()=> fetch("http://localhost:5000/users")
+  },
+  {
+    path: "/update/:id",
+    element: <Update></Update>,
+    loader: ({params})=> fetch(`http://localhost:5000/users/${params.id}`)
   },
 ]);
 
